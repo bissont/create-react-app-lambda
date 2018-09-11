01bba89006282154059745a31d4b635a3ed57cea
Tim Bisson <bissont@mac.com>
2018-09-11 10:06:54 -0700

    Return the positive and negative words as a list:
    * Create a list of the words and display them
    * remove unused handleclick

--- src/App.js
+++ src/App.js
@@ -1,52 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class LambdaDemo extends Component {
   constructor(props) {
     super(props);
-    this.state = {loading: false, msg: null};
-    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = { loading: false, msg: null, pos: [], neg: [] };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleClick = (e) => {
-    console.log("hello from handle click")
+  /*  handleClick = e => {
+    console.log("hello from handle click");
     e.preventDefault();
 
-    this.setState({loading: true});
-    fetch('/.netlify/functions/hello')
+    this.setState({ loading: true });
+    fetch("/.netlify/functions/hello")
       .then(response => response.json())
-      .then(json => this.setState({loading: false, msg: json.msg}));
-    console.log("outfrom handle click")
-  }
+      .then(json => this.setState({ loading: false, msg: json.msg }));
+    console.log("outfrom handle click");
+  };*/
 
   handleSubmit(e) {
-    console.log("here in handleSubmit")
+    console.log("here in handleSubmit");
     e.preventDefault();
-     this.setState({loading: true, msg: null});
-    const data = new FormData(e.target)
+    this.setState({ loading: true, msg: null });
+    const data = new FormData(e.target);
     for (var pair of data.entries()) {
-      console.log("hello " + pair[0] +', '+ pair[1])
+      console.log("hello " + pair[0] + ", " + pair[1]);
     }
-    fetch('/.netlify/functions/hello?to='+pair[1])
-    .then(response => response.json())
-    .then(json => {
-        this.setState({msg: json.msg });
-    })
+    fetch("/.netlify/functions/hello?to=" + pair[1])
+      .then(response => response.json())
+      .then(json => {
+        this.setState({ msg: json.msg });
+        this.setState({ pos: json.pos });
+        this.setState({ neg: json.neg });
+      });
   }
-//-          <img src="/happy.jpg" width="100"></img>
+  //-          <img src="/happy.jpg" width="100"></img>
 
-  function 
   render() {
-    const {msg,loading} = this.state;
-
-    return <div>
-      <form name="get-url" onSubmit={this.handleSubmit}>
-      <input id="theurl" name="theurl" type="text" />
-      <button type="submit">Get sentiment</button>
-      </form> 
-      <span>sentiment: {msg}</span>
+    const { msg, neg, pos } = this.state;
+    const style = "display: inline-block; border: 1px solid; float: left;";
+    return (
+      <div>
+        <form name="get-url" onSubmit={this.handleSubmit}>
+          <input id="theurl" name="theurl" type="text" />
+          <button type="submit">Get sentiment</button>
+        </form>
+        <span>sentiment: {msg}</span>
+        <br />
+        <table style={{ width: "50%", float: "left" }}>
+          <thead style={{ width: "50%", float: "left" }}>
+            <tr style={{ width: "50%", float: "left" }}>
+              <th style={{ width: "50%", float: "left" }}>positive</th>
+            </tr>
+          </thead>
+          <tbody>
+            {pos.map(word => (
+              <tr style={{ width: "50%", float: "left" }}>
+                <td>{word}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <table style={{ width: "50%", float: "left" }}>
+          <thead style={{ width: "50%", float: "left" }}>
+            <tr style={{ width: "50%", float: "left" }}>
+              <th style={{ width: "50%", float: "left" }}>negative</th>
+            </tr>
+          </thead>
+          <tbody>
+            {neg.map(word => (
+              <tr style={{ width: "50%", float: "left" }}>
+                <td>{word}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
+    );
   }
 }
 
@@ -57,8 +89,8 @@ class App extends Component {
         <header className="App-header">
           <h1 className="App-title">Airbnb user sentiment analysis</h1>
         </header>
-        <p></p>
-        <LambdaDemo/>
+        <p />
+        <LambdaDemo />
       </div>
     );
   }
--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,47 +1,56 @@
-const cheerio = require ('cheerio')
-var request = require('request')
-var Sentiment = require('sentiment');
+const cheerio = require("cheerio");
+var request = require("request");
+var Sentiment = require("sentiment");
 var sentiment = new Sentiment();
-var URI = require('urijs');
+var URI = require("urijs");
 
 export function handler(event, context, callback) {
   console.log("bar");
-  var destination = event.queryStringParameters['to']
+  var destination = event.queryStringParameters["to"];
   console.log("entiering" + destination);
-  var score = 0
-  var array = destination.split('/');
-  var id = array[array.length-1]
-  var nurl ="https://www.airbnb.com/users/review_page/" + id + "?page=";
-  console.log(nurl)
-  getData(nurl,1, function(foo) {
-      //console.log (foo.split(" ").length)
-  //    console.log (foo)
-     var score = sentiment.analyze(foo).comparative;
-      callback(null, {
+  var score = 0;
+  var array = destination.split("/");
+  var id = array[array.length - 1];
+  var nurl = "https://www.airbnb.com/users/review_page/" + id + "?page=";
+  console.log(nurl);
+  getData(nurl, 1, function(foo) {
+    //console.log (foo.split(" ").length)
+    //    console.log (foo)
+    var score = sentiment.analyze(foo).comparative;
+    var positive = sentiment.analyze(foo).positive;
+    var negative = sentiment.analyze(foo).negative;
+    //console.log("positive:" + positive);
+    //console.log("negative:" + negative);
+
+    callback(null, {
       statusCode: 200,
-      body: JSON.stringify({msg: score})
-    })
+      body: JSON.stringify({
+        msg: score,
+        pos: positive,
+        neg: negative
+      })
+    });
   });
-  
-  
 }
 
 function getData(url, page, callback) {
-  var newurl= url + page + "&host"
-  request(newurl, function (error, response, html) {
+  var newurl = url + page + "&host";
+  request(newurl, function(error, response, html) {
     if (!error && response.statusCode == 200) {
-      var obj = JSON.parse(html)
-      console.log(obj.review_ids.length)
-      const $ = cheerio.load(obj.review_content)
-      var foo = $('.expandable-content').text().trim()
-      
+      var obj = JSON.parse(html);
+      console.log(obj.review_ids.length);
+      const $ = cheerio.load(obj.review_content);
+      var foo = $(".expandable-content")
+        .text()
+        .trim();
+
       if (obj.last_page == false) {
         // recursivelly call getData(asynch function) and issue callback with recussed
         // data + foo (current data)
-        getData(url, page +1, (recdata) => {
-          callback(foo + recdata)
-        })
-      } else  {
+        getData(url, page + 1, recdata => {
+          callback(foo + recdata);
+        });
+      } else {
         /* base case*/
         callback(foo);
       }
